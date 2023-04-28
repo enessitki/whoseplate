3aeb04c885a655dec5c1903ceda25db42f378351
Enes Sitki <enes.sitki@esetron.com.tr>
2023-04-28 16:35:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/whoseplate/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.whoseplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView4);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("usernamepass").child("username").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    mTextView.setText(String.valueOf(task.getResult().getValue()));
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
+
+    }
+
+}
