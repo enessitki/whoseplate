7f6eba804e406e4d4d9a494d4cf5cf0afaeccfe7
enessitki <enessitkii@gmail.com>
2023-05-04 22:50:26 +0300

    Some desing updates and some login control cases

--- app/src/main/java/com/example/whoseplate/MainActivity.java
+++ app/src/main/java/com/example/whoseplate/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,6 +21,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -27,6 +30,7 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     AlphaAnimation signAnimation;
     AlphaAnimation signAnimationReverse;
+
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
@@ -49,8 +53,13 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.txtUsername)
     TextInputLayout usernameText;
+    @BindView(R.id.inputUsername)
+    TextInputEditText usernameEditText;
     @BindView(R.id.txtPassword)
     TextInputLayout passwordText;
+
+    @BindView(R.id.inputPassword)
+    TextInputEditText passwordEditText;
 
     @BindView(R.id.txtSignUpMail)
     TextInputLayout emailSignUpText;
@@ -94,34 +103,55 @@ public class MainActivity extends AppCompatActivity {
 
         loginButton.setOnClickListener(v -> {
             try {
+                usernameText.setErrorEnabled(false);
+                passwordText.setErrorEnabled(false);
+//                Log.e("isValiddd?", usernameEditText.getText().toString());
+//
+//                Log.e("isValiddd?", String.valueOf(isValidEmailId(usernameEditText.getText().toString().trim())));
+
+                if(!isValidEmailId(usernameEditText.getText().toString().trim())){
+
+                    usernameText.setErrorEnabled(true);
+                    usernameText.setError("The mail format is wrong!");
+                }
+                if(passwordEditText.getText().toString().trim().length() < 6){
+                    passwordText.setErrorEnabled(true);
+                    passwordText.setError("The password cannot be shorter than 6 characters.");
+                }
+
                 mAuth.signInWithEmailAndPassword(usernameText.getEditText().getText().toString().trim(), passwordText.getEditText().getText().toString().trim())
                         .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
                                 FirebaseUser user = mAuth.getCurrentUser();
 
-                                mDatabase.child("users").child(user != null ? user.getUid() : null).child("plate").get().addOnCompleteListener(task1 -> {
-                                    if (!task1.isSuccessful()) {
+                                mDatabase.child("users").child(user != null ? user.getUid() : null).child("plate").get().addOnCompleteListener(signInTask -> {
+                                    if (!signInTask.isSuccessful()) {
+                                        Log.e("sign in error",String.valueOf(signInTask.getException()));
 
                                         Toast.makeText(MainActivity.this, "Error",
                                                 Toast.LENGTH_SHORT).show();
 
                                     } else {
 
-                                Toast.makeText(MainActivity.this, "Welcome "+ task1.getResult().getValue(),
+                                Toast.makeText(MainActivity.this, "Welcome "+ signInTask.getResult().getValue(),
                                         Toast.LENGTH_SHORT).show();
 
                                     }
                                 });
 
                             } else {
-
                                 Toast.makeText(MainActivity.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
 
                             }
                         });
             } catch (Exception e) {
-                Toast.makeText(MainActivity.this, "Hata",
+
+//                if (e.toString().contains("Given String is empty or null")) {
+//                    Toast.makeText(MainActivity.this,"Please Enter Empty Lines",Toast.LENGTH_SHORT).show();
+//                }
+//                Log.e("error catch",e.toString());
+                Toast.makeText(MainActivity.this, e.toString(),
                         Toast.LENGTH_SHORT).show();
 
             }
@@ -142,8 +172,8 @@ public class MainActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(v -> {
             try {
                 mAuth.createUserWithEmailAndPassword(emailSignUpText.getEditText().getText().toString().trim(), passwordSignUpText.getEditText().getText().toString().trim())
-                        .addOnCompleteListener(this, task -> {
-                            if (task.isSuccessful()) {
+                        .addOnCompleteListener(this, signUpTask -> {
+                            if (signUpTask.isSuccessful()) {
                                 Toast.makeText(MainActivity.this, "Authentication added.",
                                         Toast.LENGTH_SHORT).show();
 
@@ -151,9 +181,12 @@ public class MainActivity extends AppCompatActivity {
                                 writeNewUser(user != null ? user.getUid() : null, plateSignUpText.getEditText().getText().toString().trim(), passwordSignUpText.getEditText().getText().toString().trim());
 
                             } else {
-                                Log.i("err", String.valueOf(task.getException()));
-                                if (String.valueOf(task.getException()).contains("The email address is already in use by another account.")){
+                                Log.i("err", String.valueOf(signUpTask.getException()));
+                                if (String.valueOf(signUpTask.getException()).contains("The email address is already in use by another account.")){
                                     emailSignUpText.setError("The email address is already in use.");
+                                }
+                                else if(String.valueOf(signUpTask.getException()).contains("Password")){
+
                                 }
 //                                    Toast.makeText(MainActivity.this, "The email adress is already in use by another account.",
 //                                            Toast.LENGTH_SHORT).show();
@@ -210,6 +243,16 @@ public class MainActivity extends AppCompatActivity {
         signAnimationReverse = new AlphaAnimation(0, 1f);
         signAnimationReverse.setDuration(1000);
         signAnimationReverse.setFillAfter(false);
+    }
+
+    private boolean isValidEmailId(String email){
+
+        return Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(email).matches();
     }
 
 
